acf9e6acd0bdd4a67b40bda0babc57d7c55a1789
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-09-25 17:49:03 -0400

    Sign Up button added

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
-    const links = document.querySelectorAll('.nav-links li');
+    // const links = document.querySelectorAll('.nav-links li');
+
+    const signUpBtn = document.querySelector("btn#signUp");
+    const blur = document.querySelector("div#blur");
+    const box = document.querySelector("#blur>div");
+
     console.log(navLinks);
 
     hamburger.addEventListener("click", function(){
         navLinks.classList.toggle('open');
     });
+
+    signUpBtn.addEventListener("click", function(){
+        signUpBtn.style.display = "none";
+        blur.style.display = "";
+        blur.style.backgroundColor = "rgba(0,0,0,0.31)";
+        box.style.backgroundColor = "red";
+    })
   });
 

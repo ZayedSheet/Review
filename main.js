98516eddf3f8a5603fe3c40b3adbfc3d78b02191
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-09-27 19:07:51 -0400

    Added setBlur() and setButtons() functions to hide/show different elements.

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){
     const navLinks = document.querySelector('.nav-links');
     // const links = document.querySelectorAll('.nav-links li');
 
-    const signUpBtn = document.querySelector(".signUp");
-    const closeBtn = document.querySelector(".close");
+    //btn[0] = Login, btn[1] = SignUp, btn[2] = close
+    const btn = document.querySelectorAll("btn");
+
     const blur = document.querySelector("div#blur");
     const box = document.querySelector("#blur>div");
+    const form = document.querySelector("form");
 
     console.log(navLinks);
 
@@ -14,21 +16,48 @@ document.addEventListener("DOMContentLoaded", function(){
         navLinks.classList.toggle('open');
     });
 
-    signUpBtn.addEventListener("click", function(){
-        signUpBtn.style.display = "none";
-        closeBtn.style.display = "inline";
-        closeBtn.style.backgroundColor = "rgba(63, 179, 255, 0.5)";
-        blur.style.backgroundColor = "rgba(0,0,0,0.31)";
-        box.style.backgroundColor = "rgba(255,255,255,0.75)";
+    /*   LOGIN BUTTON    */
+    //
+    // btn[0].addEventListener("click", function () {
+    //
+    // });
+
+
+    /*   SIGN UP BUTTON   */
+    btn[1].addEventListener("click", function(){
+        btn[2].style.display = "inline";
+        form.style.display = "inline";
+        setButtons(false);
+        setBlur(true);
     });
 
-    closeBtn.addEventListener("click", function(){
-        signUpBtn.style.display = "inline";
-        closeBtn.style.display = "none";
-        closeBtn.style.backgroundColor = "rgba(63, 179, 255, 0)";
-        blur.style.backgroundColor = "rgba(0,0,0,0)";
-        box.style.backgroundColor = "rgba(255,255,255,0.75)";
+    btn[2].addEventListener("click", function(){
+        btn[2].style.display = "none";
+        form.style.display = "none";
+        setBlur(false);
+        setButtons(true);
     });
 
+    function setButtons(condition){
+        if(condition){
+            btn[0].style.display = "inline";
+            btn[1].style.display = "inline";
+        }else{
+            btn[0].style.display = "none";
+            btn[1].style.display = "none";
+        }
+    }
+
+    function setBlur(condition){
+        if(condition){
+            btn[2].style.backgroundColor = "rgba(63, 179, 255, 0.5)";
+            blur.style.backgroundColor = "rgba(0,0,0,0.31)";
+            box.style.backgroundColor = "rgba(255,255,255,0.75)";
+        }else{
+            btn[2].style.backgroundColor = "rgba(63, 179, 255, 0)";
+            blur.style.backgroundColor = "rgba(0,0,0,0)";
+            box.style.backgroundColor = "rgba(255,255,255,0.75)";
+        }
+    }
   });
 

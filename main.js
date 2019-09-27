13e449a795366b5c17fa9559ac408a78126b4be4
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-09-27 16:16:09 -0400

    Added close functionality via close button

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     // const links = document.querySelectorAll('.nav-links li');
 
     const signUpBtn = document.querySelector(".signUp");
-    // const closeBtn = document.getElementById("close");
+    const closeBtn = document.querySelector(".close")
     const blur = document.querySelector("div#blur");
     const box = document.querySelector("#blur>div");
 
@@ -16,11 +16,19 @@ document.addEventListener("DOMContentLoaded", function(){
 
     signUpBtn.addEventListener("click", function(){
         signUpBtn.style.display = "none";
-        // closeBtn.style.display = "";
-        blur.style.display = "";
+        closeBtn.style.display = "inline";
+        closeBtn.style.backgroundColor = "rgba(63, 179, 255, 0.5)";
         blur.style.backgroundColor = "rgba(0,0,0,0.31)";
         box.style.backgroundColor = "rgba(255,255,255,0.75)";
-    })
+    });
+
+    closeBtn.addEventListener("click", function(){
+        signUpBtn.style.display = "inline";
+        closeBtn.style.display = "none";
+        closeBtn.style.backgroundColor = "rgba(63, 179, 255, 0)";
+        blur.style.backgroundColor = "rgba(0,0,0,0)";
+        box.style.backgroundColor = "rgba(255,255,255,0)";
+    });
 
   });
 

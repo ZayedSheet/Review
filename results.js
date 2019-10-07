838bc0fe0b1fe726100d1375923f523a7750ddab
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-10-07 18:31:31 -0400

    Added account button in place of signIn/logIn buttons. Commented results.html

--- results.js
+++ results.js
@@ -6,13 +6,17 @@ document.addEventListener("DOMContentLoaded", function(){
     const toggleSwitch = document.querySelector(".toggle-switch");
     const mapView = document.querySelector(".map-view");
 
+
     hamburger.addEventListener("click", function(){
         navLinks.classList.toggle('open');
     });
 
+    /**
+     * Toggles between list and map view
+     */
     toggleView.addEventListener("click", function () {
         toggleSwitch.classList.toggle("toggle-map");
         mapView.classList.toggle("map-view-enable");
-    })
-
+    });
+    
 });

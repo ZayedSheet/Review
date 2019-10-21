44b4cab1712f9ea0cfcb3e2987964d56fca437b2
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-10-21 17:31:22 -0400

    Fixed search button style, Added live map to results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,98 +1,115 @@
 import React from 'react';
-import SigninLoginButton from "./Buttons/SigninLoginButton";
-import SearchBar from "./Search/SearchBar";
+import './results.css'
+import MapContainer from "./MapContainer";
 
 const Home = () => {
+
     return (
-        <div id="list-view">
-            {/*        Each element in the list is a search result*/}
-            <ul>
-                <li className="list-item" onClick="window.location.href='object.html'">
-                    <div className="item-image" id="item1"></div>
-                    <div>
-                        <h2>Niagara Region
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                        </h2>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
-                            officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
+        <>
+            <div id="list-view">
+                {/*        Each element in the list is a search result*/}
+                <ul>
+                    <li className="list-item" onClick="window.location.href='object.html'">
+                        <div className="item-image" id="item1"></div>
+                        <div>
+                            <h2>Niagara Region
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                            </h2>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
+                                officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div className="item-image" id="item2"></div>
+                        <div>
+                            <h2>Hamilton
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                            </h2>
+                            <p> Hamilton features many types of factories.
+                                Big factories, small factories, and even medium
+                                factories.</p>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div className="item-image" id="item3"></div>
+                        <div>
+                            <h2>Barrie
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                            </h2>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
+                                officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div className="item-image" id="item4"></div>
+                        <div>
+                            <h2>Barrie
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                            </h2>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
+                                officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div className="item-image" id="item4"></div>
+                        <div>
+                            <h2>Barrie
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                            </h2>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
+                                officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div className="item-image" id="item4"></div>
+                        <div>
+                            <h2>Barrie
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                                <i className="fas fa-star star"></i>
+                            </h2>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
+                                officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="map-view">
+                <MapContainer></MapContainer>
+            </div>
+
+            <div id="toggle-background">
+                <p className="toggle noselect">LIST</p>
+                <div className="toggle">
+                    <div className="toggle-switch">
                     </div>
-                </li>
-                <li className="list-item">
-                    <div className="item-image" id="item2"></div>
-                    <div>
-                        <h2>Hamilton
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                        </h2>
-                        <p> Hamilton features many types of factories.
-                            Big factories, small factories, and even medium
-                            factories.</p>
-                    </div>
-                </li>
-                <li className="list-item">
-                    <div className="item-image" id="item3"></div>
-                    <div>
-                        <h2>Barrie
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                        </h2>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
-                            officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
-                    </div>
-                </li>
-                <li className="list-item">
-                    <div className="item-image" id="item4"></div>
-                    <div>
-                        <h2>Barrie
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                        </h2>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
-                            officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
-                    </div>
-                </li>
-                <li className="list-item">
-                    <div className="item-image" id="item4"></div>
-                    <div>
-                        <h2>Barrie
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                        </h2>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
-                            officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
-                    </div>
-                </li>
-                <li className="list-item">
-                    <div className="item-image" id="item4"></div>
-                    <div>
-                        <h2>Barrie
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                            <i className="fas fa-star star"></i>
-                        </h2>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus fugit in ipsam
-                            officiis, repellendus? Asperiores consequuntur deserunt ea id.</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+                </div>
+                <p className="toggle noselect">MAP</p>
+            </div>
+
+        </>
     );
 }
 
--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -4,13 +4,13 @@ import {NavLink} from "react-router-dom";
 
 const SearchBar = () => {
     return(
-        <form className={"search"} action="/action_page.php">
+        <form className={"search"}>
             <input type="text" placeholder="Search.." name="search" />
-                <NavLink class="search-button" to="/Results" type="button">
+                <NavLink className="search-button" to="/Results" type="button">
                     <i className="fa fa-search"></i>
                 </NavLink>
         </form>
     );
-}
+};
 
 export default SearchBar;

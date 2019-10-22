36ed4dacacb9c09ab86fca0be9dc0cd5334e4c1e
Zayed <Zayed.Sheet@gmail.com>
2019-10-21 23:21:08 -0400

    added area component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Submission from './components/Submission';
 import Results from './components/Results';
+import Area from './components/Area';
 
 import NavBar from './components/Nav/Navbar';
 import Footer from './components/Footer/Footer';
@@ -18,6 +19,7 @@ const App = () => {
               <Route path="/" component={Home} exact/>
               <Route path="/Submission" component={Submission}/>
               <Route path="/Results" component={Results}/>
+              <Route path="/Area" component={Area}/>
             </Switch>
             <Footer/>
         </div>

e7824ae89d7c57ce6fafcd8f860a9b3e3f4ef405
Zayed <Zayed.Sheet@gmail.com>
2019-10-23 02:13:35 -0400

    edit

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import "./SearchBar.css";
+/*
+Nav Link Component allows links to be added to change the current page.
+In other words, change the component in the App.js switch statement
+ */
 import {NavLink} from "react-router-dom";
 
+/**
+ * General search bar to search the website for objects
+ * @returns SearchBar component
+ */
 const SearchBar = () => {
+    /*
+    Variables for geolocation functionality,
+    coordinates set to false until user searches by location
+     */
     let locationToggled = false;
     let longitude = false;
     let latitude = false;
 
+    /**
+     * Toggles to colour on the location search option in the search bar depending on if it is selected
+     * Sets locationToggled to true if the option is enabled
+     * Initializes the coordinates of the user via the getPosition function
+     */
     const isToggled = () => {
         if (navigator.geolocation && !locationToggled){
             navigator.geolocation.getCurrentPosition(getPosition);
@@ -17,6 +34,11 @@ const SearchBar = () => {
         }
     };
 
+    /**
+     * Initializes the coordinates of the user, and displays them to the user
+     * @param position The longitude and latitude of the user,
+     * obtained from the geoLoaction api.
+     */
     function getPosition(position) {
         longitude = position.coords.longitude;
         latitude = position.coords.latitude;
@@ -25,16 +47,19 @@ const SearchBar = () => {
     }
 
     return(
+        //Container for the entire search bar
         <form className={"search"}>
+            {/*Input text field*/}
             <input type="text" placeholder="Search.." name="search" />
+            {/*Container for the search by location button,
+            when clicked it toggles search by location and sets the coords*/}
             <div onClick={isToggled} title="Click me to toggle search by location!" className={"search-location-button"}>
                 <i className="fas fa-location-arrow"/>
             </div>
-                <NavLink className="search-button" to={{
-                    pathname: 
-                }} type="button">
-                    <i className="fa fa-search"/>
-                </NavLink>
+            {/*Search button*/}
+            <NavLink className="search-button" to="/Results" type="button">
+                <i className="fa fa-search"/>
+            </NavLink>
         </form>
     );
 };

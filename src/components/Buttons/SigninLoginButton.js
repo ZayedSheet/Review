b8c96b02aa11d3582491cba5d1779963dbaa4df4
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-10-23 02:21:44 -0400

    Added comments to login/signup button component

--- src/components/Buttons/SigninLoginButton.js
+++ src/components/Buttons/SigninLoginButton.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './SigninLoginButton.css'
 
+/**
+ * Sign and login button
+ * @param props properties of the component,
+ *        in this case props.formName is the form that the button toggles (either sign in or login form)
+ * @returns A sign in or login Component (depending on formName property)
+ */
 const SigninLoginButton = (props) => {
 
+    /**
+     * Toggles visibility of the corresponding form (sign in or login form)
+     */
     const toggleForm = () => {
         document.getElementById(props.formName).classList.toggle(props.formName);
     }
 
     return(
+      //  When the button is clicked, the corresponding form appears on screen
       <button onClick={toggleForm} className={`button-style`}>
+          {/*The text inside the button, will be the components children
+          (the text between the components open and closing tag)*/}
           {props.children}
       </button>
     );

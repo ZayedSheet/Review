603e0202300c50be2b689460f765fc7eb3d562b5
Zayed <Zayed.Sheet@gmail.com>
2019-10-23 01:48:07 -0400

    added search by location

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -3,11 +3,35 @@ import "./SearchBar.css";
 import {NavLink} from "react-router-dom";
 
 const SearchBar = () => {
+    let locationToggled = false;
+    let longitude = false;
+    let latitude = false;
+
+    const isToggled = () => {
+        if (navigator.geolocation && !locationToggled){
+            navigator.geolocation.getCurrentPosition(getPosition);
+            document.querySelector(".search .fa-location-arrow").style.color = "#0b7dda";
+            locationToggled = true;
+        }
+        else{
+            document.querySelector(".search .fa-location-arrow").style.color = "#cccccc";
+            locationToggled = false;
+        }
+    };
+
+    function getPosition(position) {
+        longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
+    }
+
     return(
         <form className={"search"}>
             <input type="text" placeholder="Search.." name="search" />
+            <div onClick={isToggled} title="Click me to toggle search by location!" className={"search-location-button"}>
+                <i className="fas fa-location-arrow"/>
+            </div>
                 <NavLink className="search-button" to="/Results" type="button">
-                    <i className="fa fa-search"></i>
+                    <i className="fa fa-search"/>
                 </NavLink>
         </form>
     );

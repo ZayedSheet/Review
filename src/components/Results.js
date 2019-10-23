1cd0ed1c5ed8d58f0bda25da114c8b0ccda5578e
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-10-23 03:54:18 -0400

    Results Component commented

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './results.css'
+
+//Components within the results page
 import MapContainer from "./MapContainer";
 import ListItem from "./Submission/ListItem";
 
+/**
+ * Toggles map view in mobile version of the results page
+ */
 const toggleMap = () => {
     document.querySelector(".toggle-switch").classList.toggle('toggle-map');
     document.querySelector(".map-view").classList.toggle('map-view-enable');
 };
 
-const Home = (props) => {
-
-    // console.log(props.location.locationProp);
-
+/**
+ * Page that displays search results
+ * @param props properties of the component
+ *              props.location.locationProps is used by the searchBar to center the map if searched by location
+ * @returns A Results Component
+ */
+const Results = (props) => {
     return (
+        //Empty container for all elements as JSX does not allow adjacent elements
         <>
+            {/*Container for the list view of the search results*/}
             <div id="list-view">
-                {/*        Each element in the list is a search result*/}
+
+                {/* Each element in the list is a search result displayed as a ListItem Component*/}
                 <ul>
                     <ListItem to="../Area" id="item1" stars={4} title={"Niagara Region"}>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, optio!
@@ -40,12 +51,16 @@ const Home = (props) => {
                 </ul>
             </div>
 
+            {/*Container for the map view of the search results, displayed using Google Maps API
+            The map is centered at the users location if they search by location, or a default location otherwise*/}
             <div className="map-view">
                 <MapContainer center={props.location.locationProp}/>
             </div>
 
+            {/* Toggle switch to toggle between map and list view on mobile */}
             <div id="toggle-background">
                 <p className="toggle noselect">LIST</p>
+                {/*When toggle button is clicked, the map-view is toggled via toggleMap function*/}
                 <div className="toggle" onClick={toggleMap}>
                     <div className="toggle-switch">
                     </div>
@@ -57,4 +72,4 @@ const Home = (props) => {
     );
 };
 
-export default Home;
+export default Results;

cc252641b08180af994e81c1601137eab6ba3882
Zayed <Zayed.Sheet@gmail.com>
2019-10-31 21:11:43 -0400

    Updates

---
+++ precache-manifest.427c9ac04b12d4f3d3c04670f0206f3f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace5b6f1c76c4b722a98a594da2242fd",
+    "url": "/Review/index.html"
+  },
+  {
+    "revision": "896edf7b6b2b90f274ca",
+    "url": "/Review/static/css/main.27c48b03.chunk.css"
+  },
+  {
+    "revision": "5d7fdad4970af42c0344",
+    "url": "/Review/static/js/2.cd29dc2b.chunk.js"
+  },
+  {
+    "revision": "896edf7b6b2b90f274ca",
+    "url": "/Review/static/js/main.68cb9473.chunk.js"
+  },
+  {
+    "revision": "eac176570b05fa35ad3d",
+    "url": "/Review/static/js/runtime-main.174ca025.js"
+  },
+  {
+    "revision": "d53ab1c841cfb138502e512fe76b3fe3",
+    "url": "/Review/static/media/Logo_aligned.d53ab1c8.png"
+  },
+  {
+    "revision": "510ec7b42d6dc4a9d4acfeace3847fa1",
+    "url": "/Review/static/media/background_desktop.510ec7b4.png"
+  },
+  {
+    "revision": "e5250e564b3908059007c4e82d149bd6",
+    "url": "/Review/static/media/background_landing.e5250e56.png"
+  },
+  {
+    "revision": "afee4c1084a8a0b0b3c9d96258fb4dd6",
+    "url": "/Review/static/media/gorge.afee4c10.jpg"
+  },
+  {
+    "revision": "c8429976935eeea7d3eb8e7c4329b98f",
+    "url": "/Review/static/media/map_big.c8429976.jpg"
+  }
+]);

2222700d914cb23a16a01d14a865ce2ad052e7a5
Zayed <Zayed.Sheet@gmail.com>
2019-11-09 17:56:12 -0500

    changed variable names for login

--- frontend/src/components/Forms/LoginForm.js
+++ frontend/src/components/Forms/LoginForm.js
@@ -7,7 +7,7 @@ import './Form.css'
  * @returns {*}
  */
 const LoginForm = (props) => {
-    const {inputs, handleInputChange, handleSubmit} = useForm(); //retreives functions and state variables from form hook
+    const {inputs, nameFields, handleInputChange, handleSubmit} = useForm(); //retreives functions and state variables from form hook
 
 
     return (
@@ -17,11 +17,11 @@ const LoginForm = (props) => {
                 <h1>Login</h1>
                 <div>
                     <label>Username</label>
-                    <input type="text" name="username" onChange={handleInputChange} value={inputs.username} required/><br/>{/*Runs the handleInputChange function in form hook on input change*/}
+                    <input type="text" name={nameFields.username} onChange={handleInputChange} value={inputs.username} required/><br/>{/*Runs the handleInputChange function in form hook on input change*/}
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="userPassword" onChange={handleInputChange} value={inputs.userPassword} required/><br/>
+                    <input type="password" name={nameFields.password} onChange={handleInputChange} value={inputs.password} required/><br/>
                 </div>
                 <input type="submit" value="Login"/>
             </form>

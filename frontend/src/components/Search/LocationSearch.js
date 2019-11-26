cb8078a903bbdc44d478298ed5f82ac23bfcbbbc
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-11-26 00:24:53 -0500

    Improved autofill for submission form Can choose between autofill and manual fill

--- frontend/src/components/Search/LocationSearch.js
+++ frontend/src/components/Search/LocationSearch.js
@@ -5,35 +5,48 @@ import PlacesAutocomplete, {
     getLatLng,
 } from 'react-places-autocomplete';
 
-const LocationSearchInput = () => {
+let lookup = require('country-code-lookup');
+
+const LocationSearchInput = (props) => {
 
     const [address, setAddress] = useState('');
+    let selectedAddress;
 
     const handleChange = address => {
         setAddress( address );
     };
 
     const handleSelect = address => {
+        document.querySelector("input[name='address']").value = address;
+        setAddress(address);
         geocodeByAddress(address)
             .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
+            .then(latLng => console.log('Success', setValues(address, latLng)))
             .catch(error => console.error('Error', error));
     };
 
+    const setValues = (address, latLng) => {
+        selectedAddress = address.split(/\s*[-,]\s*/);
+        selectedAddress[selectedAddress.length - 1] = lookup.byCountry(selectedAddress[selectedAddress.length - 1]).iso2;
+        document.querySelector("input[name='longitude']").value = latLng.lng;
+        document.querySelector("input[name='latitude']").value = latLng.lat;
+        document.querySelector("input[name='city']").value = selectedAddress[0];
+        document.querySelector("select[name='country']").value = selectedAddress[selectedAddress.length - 1];
+        props.setInputs(
+            inputs => ({...inputs, city: selectedAddress[0], country: selectedAddress[2],
+                latitude: latLng.lat, longitude: latLng.lng})
+        );
+    };
+
     return (
-        <PlacesAutocomplete
-            value={address}
-            onChange={handleChange}
-            onSelect={handleSelect}
-        >
+        <PlacesAutocomplete value={address} onChange={handleChange} onSelect={handleSelect}>
             {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
                 <div>
-                    <input
+                    <input name={'address'}
                         {...getInputProps({
                             placeholder: 'Search Places ...',
                             className: 'location-search-input',
-                        })}
-                    />
+                        })}/>
                     <div className="autocomplete-dropdown-container">
                         {loading && <div>Loading...</div>}
                         {suggestions.map(suggestion => {
@@ -41,16 +54,10 @@ const LocationSearchInput = () => {
                                 ? 'suggestion-item--active'
                                 : 'suggestion-item';
                             // inline style for demonstration purpose
-                            const style = suggestion.active
-                                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                            const style = suggestion.active ? { backgroundColor: '#fafafa', cursor: 'pointer' }
                                 : { backgroundColor: '#ffffff', cursor: 'pointer' };
                             return (
-                                <div
-                                    {...getSuggestionItemProps(suggestion, {
-                                        className,
-                                        style,
-                                    })}
-                                >
+                                <div{...getSuggestionItemProps(suggestion, {className, style,})}>
                                     <span>{suggestion.description}</span>
                                 </div>
                             );

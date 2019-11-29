4e371bdc7dea9669ee427e74fba33af0324c4ecf
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-11-28 20:13:19 -0500

    Updated schema to have text fields as an index

--- backend/models/objects.model.js
+++ backend/models/objects.model.js
@@ -3,25 +3,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const objectsSchema = new Schema({
-    name: {type: String,
-            required: true,
-            unique: true
-    },
+    name: {type: String, required: true, unique: true},
     city: {type: String, required: true},
     country: {type: String, required: true},
     overview: {type: String, required: true},
     username: {type: String, required: true},
     coordinates: {
-            latitude: {
-                type: Number,
-                required: true
-            },
-            longitude: {
-                type: Number,
-                required: true
-            }
+            latitude: {type: Number, required: true},
+            longitude: {type: Number, required: true}
     }
 });
+
+objectsSchema.index({name: 'text', city: 'text', country: 'text', overview: 'text'});
 
 const Objects = mongoose.model('Object', objectsSchema);
 module.exports = Objects;

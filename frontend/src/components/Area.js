048c826e0eb757840f05fcbfb9c8e26027715a4e
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-11-30 21:33:40 -0500

    fixed spaces photo url

--- frontend/src/components/Area.js
+++ frontend/src/components/Area.js
@@ -8,7 +8,9 @@ import AddReviewForm from "./Forms/AddReviewForm";
 
 const Area = (props) => {
 
-    let photoUrl = "https://review-bucket-react.s3.us-east-2.amazonaws.com/" + props.match.params.name + "/cover.png";
+    let photoUrl = "https://review-bucket-react.s3.us-east-2.amazonaws.com/" + props.match.params.name.replace(" ", "+") + "/cover.png";
+    console.log(photoUrl);
+
     const [area, setArea] = useState({});
     const [reviews, setReviews] = useState(false);
     const test = [

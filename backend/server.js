a0690b83fef96f3717a660a47257108ab96e9796
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-12-07 03:47:39 -0500

    Https Backend

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,7 @@ app.get("*", (req,res) => {
 });
 
 const server = https.createServer(options, app);
+server.listen(8443);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);

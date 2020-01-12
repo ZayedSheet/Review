36d749a7a449f79d72be4d231ac602a67e3b539c
Dominik Buszowiecki <DominikB2014@gmail.com>
2020-01-11 21:17:27 -0500

    Removed unused generateGetURL for AWS Presigner

--- backend/AWSPresigner.js
+++ backend/AWSPresigner.js
@@ -14,32 +14,8 @@ const s3 = new AWS.S3();
 // Retrieving the bucket name from env variable
 const Bucket = process.env.BUCKET_NAME;
 
-// In order to create pre-signed GET adn PUT URLs we use the AWS SDK s3.getSignedUrl method.
-// getSignedUrl(operation, params, callback) ⇒ String
-// For more information check the AWS documentation: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
-
-// GET URL Generator
-function generateGetUrl(Key) {
-    return new Promise((resolve, reject) => {
-        const params = {
-            Bucket,
-            Key,
-            Expires: 120 // 2 minutes
-        };
-        // Note operation in this case is getObject
-        s3.getSignedUrl('getObject', params, (err, url) => {
-            if (err) {
-                reject(err);
-            } else {
-                // If there is no errors we will send back the pre-signed GET URL
-                resolve(url);
-            }
-        });
-    });
-}
-
 // PUT URL Generator
-function generatePutUrl(Key, ContentType) {
+const generatePutUrl = (Key, ContentType) => {
     return new Promise((resolve, reject) => {
         // Note Bucket is retrieved from the env variable above.
         const params = { Bucket, Key, ContentType };
@@ -52,7 +28,7 @@ function generatePutUrl(Key, ContentType) {
             resolve(url);
         });
     });
-}
+};
 
 // Finally, we export the methods so we can use it in our main application.
-module.exports = { generateGetUrl, generatePutUrl };
+module.exports = {generatePutUrl };

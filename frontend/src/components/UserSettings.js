972036b8939b0fcce6ddfbdf50d266a8fdb9c344
Dominik Buszowiecki <DominikB2014@gmail.com>
2020-01-20 18:05:52 -0500

    Created Settings Item Component

--- frontend/src/components/UserSettings.js
+++ frontend/src/components/UserSettings.js
@@ -1,43 +1,43 @@
 import React, {useContext} from 'react'
 import UserContext from "../UserContext";
 
-const UserSettings = () => {
-
-    const {user} = useContext(UserContext);
+const SettingsItem = (props) => {
 
     const handleClick = (event) => {
         console.log(event.target.name);
     };
-
     const divStyle={position:"relative", margin: "12px 0",};
-    const textStyle = {padding: "20px 0"};
     const buttonStyle = {position: "absolute", right: 0, top: "50%", transform: "translateY(-50%)"};
 
+    let button = (<button name={props.name} onClick={handleClick} style={buttonStyle}>{props.btnText}</button>);
+    console.log(props.noBtn);
+    if(props.noBtn) button = null;
+
+    return(
+        <div style={divStyle}>
+            <h3>{props.children}</h3>
+            {button}
+            <p>{props.value}</p>
+        </div>
+    )
+};
+
+const UserSettings = () => {
+
+    const {user} = useContext(UserContext);
+    const textStyle = {padding: "20px 0"};
 
     return(
             <div style={{margin: "100px 0"}}>
                 <h1 style={{textAlign: "center"}}>Account Settings</h1>
                 <div style={{width: "50%", margin: "auto"}}>
-                    <div style={divStyle}>
-                        <button name={"Username"} onClick={handleClick} style={buttonStyle}>Change Username</button>
-                        <h3>Username: </h3>
-                        <p>{user.username}</p>
-                    </div>
-                    <div style={divStyle}>
-                        <button name={"Email"} onClick={handleClick} style={buttonStyle}>Change Email</button>
-                        <h3>Email: </h3>
-                        <p>{user.email}</p>
-                    </div>
-                    <div style={divStyle}>
-                        <button name={"Name"} onClick={handleClick} style={buttonStyle}>Change Email</button>
-                        <h3>Name:</h3>
-                        <p>{user.name}</p>
-                    </div>
-                    <div style={divStyle}>
-                        <button name={"Verified"} onClick={handleClick} style={buttonStyle}>Verify Email</button>
-                        <h3>Email Verified: </h3>
-                        <p>{user.verified ? 'Yes' : 'No'}</p>
-                    </div>
+                    <SettingsItem name={"Username"} btnText={"Change Username"} value={user.username}>Username: </SettingsItem>
+                    <SettingsItem name={"Email"} btnText={"Change Email"} value={user.email}>Email: </SettingsItem>
+                    <SettingsItem name={"Name"} btnText={"Change Name"} value={user.name}>Name: </SettingsItem>
+                    <SettingsItem name={"Verified"} btnText={"Verify Email"} value={user.verified ? 'Yes' : 'No'}
+                                  noBtn={user.verified}>
+                        Email Verified:
+                    </SettingsItem>
                 </div>
             </div>
     )

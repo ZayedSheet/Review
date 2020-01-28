189f624a0fb5268448d3ff9065758ec14a1211f3
Dominik Buszowiecki <DominikB2014@gmail.com>
2020-01-27 21:38:37 -0500

    Updated UserSettings page to use CSS Modules

--- frontend/src/components/UserSettings.js
+++ frontend/src/components/UserSettings.js
@@ -1,48 +1,51 @@
 import React, {useContext, useState} from 'react'
 import UserContext from "../UserContext";
+import classes from './UserSettings.module.css';
 
-const PasswordVerifyForm = () => {
+const PasswordVerifyForm = (props) => {
 
-    const handleSubmit = () => {
+    //
+    // const [values, setValues] = setState({update: {[props.field]: 'fieldName'}})
 
-    };
 
     return (
         <div className={"blur-background"}>
-            <form onSubmit={handleSubmit} id={"pop-up-form"} className={"form-style"}>{/*Runs the handleSubmit function from form hook when submit button is clicked*/}
-                <span className="close"/>{/*Runs the toggleForm function on click to close the form*/}
+            <form id={"pop-up-form"} className={"form-style"}>
+                <span className="close"/>
                 <h1>Verify Password</h1>
                 <div>
                     <label>Password</label>
-                    <input autoComplete={"password"} type="password" required/><br/>
+                    <input autoComplete={"password"} name="password" type="password" required/><br/>
                 </div>
                 <input type="submit" value="Continue"/>
             </form>
         </div>
     );
-}
+};
 
 const SettingsItem = (props) => {
     /****  Styles and Values  ****/
-    const divStyle={position:"relative", margin: "12px 0",};
-    const buttonStyle = {position: "absolute", right: 0, top: "50%", transform: "translateY(-50%)"};
     let verifyPass = null;
 
     /****  States  ****/
     const [isEdit, setEdit] = useState(false); //If the user is editing the field
+    const [isVerifyPass, setVerifyPass] = useState(false);
 
     /****  Functions  ****/
     const handleClick = (event) => {
-        if (!isEdit){
-
-        }
-        if(event.target.name !== "verified") setEdit(!isEdit);
+        event.preventDefault();
+        if(event.target.name !== "verified") {setEdit(!isEdit)};
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setVerifyPass(true);
     };
 
+    if(isVerifyPass) verifyPass = <PasswordVerifyForm field={props.name}/>;
 
     /**** Buttons and Button Logic ****/
-    let button = (<button name={props.name} onClick={handleClick} style={buttonStyle}>{props.btnText}</button>);
-    if(isEdit) button = (<button name={props.name} onClick={handleClick} style={buttonStyle}>Save</button>);
+    let button = (<button type={"button"} name={props.name} onClick={handleClick} className={classes.ButtonStyle}>{props.btnText}</button>);
+    if(isEdit) button = (<button type={"submit"} name={props.name} className={classes.ButtonStyle}>Save</button>);
     if(props.noBtn) button = null;
 
     /****  Edit Input Field  ****/
@@ -50,10 +53,12 @@ const SettingsItem = (props) => {
 
 
     return(
-        <div style={divStyle}>
+        <div className={classes.DivStyle}>
             <h3>{props.children}</h3>
-            <div>{button}</div>
-            <p>{isEdit ? input: props.value}</p>
+            <form onSubmit={handleSubmit}>
+                <div>{button}</div>
+                <p>{isEdit ? input: props.value}</p>
+            </form>
             {verifyPass}
         </div>
     )
@@ -67,10 +72,10 @@ const UserSettings = (props) => {
     const textStyle = {padding: "20px 0"};
 
     return(
-            <div style={{margin: "100px 0"}}>
-                <h1 style={{textAlign: "center"}}>Account Settings</h1>
-                <div style={{width: "50%", margin: "auto"}}>
-                    <SettingsItem name={"username"} btnText={"Change Username"} value={user.username}>Username: </SettingsItem>
+            <div className={classes.UserSettings}>
+                <h1>Account Settings</h1>
+                <div>
+                    <SettingsItem name={"username"} btnText={"Change Username"} value={user.username} noBtn>Username: </SettingsItem>
                     <SettingsItem name={"email"} btnText={"Change Email"} value={user.email}>Email: </SettingsItem>
                     <SettingsItem name={"name"} btnText={"Change Name"} value={user.name}>Name: </SettingsItem>
                     <SettingsItem name={"verified"} btnText={"Verify Email"} value={user.verified ? 'Yes' : 'No'}

3e0891bb350d5ed764c96a065af2bacdb778b515
Dominik Buszowiecki <buszowid@mcmaster.ca>
2020-09-26 21:41:25 -0400

    Cleanup Area page

--- frontend/src/components/Gallery/Gallery.js
+++ frontend/src/components/Gallery/Gallery.js
@@ -9,6 +9,7 @@ const Image = (props) => {
         setEnlarge(true);
         if(props.setOpen) props.setOpen(props.key);
     };
+
     const handleClose = () => {
         setEnlarge(false);
     };
@@ -16,13 +17,24 @@ const Image = (props) => {
     useEffect(() => {if(props.setOpen) props.open === props.key ? handleOpen(): handleClose()}, [props.open]);
 
     let largeStyle = isEnlarge ? {
-            position: "fixed", cursor: "default", display: "block", border:"1px solid black",
-            top: "0px",marginLeft:"20%", marginTop:"20vh", left: "0px", width: "60vw", height: "60vh", zIndex: "100", ...props.style}
-        : props.style;
+            position: "fixed",
+            cursor: "default",
+            display: "block",
+            border:"1px solid black",
+            top: "0px",
+            marginLeft:"20%",
+            marginTop:"20vh",
+            left: "0px",
+            width: "60vw",
+            height: "60vh",
+            zIndex: "100",
+            ...props.style
+        } : props.style;
 
     return(
         <div onClick={isEnlarge ? null : handleOpen}
-             className={props.className} style={largeStyle}>
+            className={props.className} style={largeStyle}
+        >
             {isEnlarge ?
                 <>
                     <span style={{position: "fixed",right:"21%", top:"22vh", zIndex: "101"}} onClick={() => {handleClose(); if(props.setOpen) props.setOpen(-1);}} className="close"/>
@@ -36,9 +48,14 @@ const Image = (props) => {
 let Gallery = (props) => {
     const [isOpen, setOpen] = useState(-1);
     let i = 0;
+
     const [images, setImages] = useState(
-        props.children.map((img) => {return {...img, props: {...img.props, setOpen: setOpen, open: isOpen, key: i++}}})
-    );
+        props.children.map(img => ({
+            ...img,
+            props: {...img.props, setOpen: setOpen, open: isOpen, key: i++}
+        })
+    ));
+
     useEffect(() => {
         i = 0;
         setImages(props.children.map((img) => {return {...img, props: {...img.props, setOpen: setOpen, open: isOpen, key: i++}}})
@@ -57,9 +74,9 @@ let Gallery = (props) => {
     };
 
     let nextImageBtn =
-        (<i key="next" style={{...arrowStyle, right: "21%"}} onClick={() => changeImage(isOpen+1)} className="fas fa-arrow-right"/>);
+        (<i key="next" style={{...arrowStyle, right: "21%"}} onClick={() => changeImage(isOpen +1 )} className="fas fa-arrow-right"/>);
     let prevImageBtn =
-        (<span key="prev" style={{...arrowStyle, left: "21%"}} onClick={() => changeImage(isOpen-1)} className="fas fa-arrow-left"/>);
+        (<span key="prev" style={{...arrowStyle, left: "21%"}} onClick={() => changeImage(isOpen -1 )} className="fas fa-arrow-left"/>);
 
     return(
         <>
